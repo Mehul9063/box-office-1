4c1ce91ca192c6b8e9b6177567776669e350c085
Mehul9063 <mehulpotnuru1@gmail.com>
2021-06-09 12:37:41 +0530

    added loading and error messages

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -7,17 +7,41 @@ const Show = () => {
   const {id} = useParams();
 
   const [show,setShow] = useState(null);
+  const [isLoading,setIsLoading] = useState(true)
+  const [error,setError] = useState(null)
   
   useEffect ( ()=> {
 
+    let isMounted=true
+    
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results)
+        if(isMounted) {
+          setShow(results)
+          setIsLoading(false)
+        }
     })
+    .catch(err => {
+      if(isMounted) {
+        setError(err.message)
+        setIsLoading(false);
+      }
+    })
+
+    return () => {
+      isMounted=false;
+    }
 
   },[id])
 
   // eslint-disable-next-line no-console
   console.log ('show',show);
+  if(isLoading) {
+    return <div>Data is being loaded</div>
+  }
+
+  if(error) {
+    return <div>Error occurred: {error}</div>
+  }
   
   return (
     <div>This is show page.</div>
