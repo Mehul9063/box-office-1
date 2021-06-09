29ee78f0715944986ebdd2544c0598d3061cb807
Mehul9063 <mehulpotnuru1@gmail.com>
2021-06-09 20:10:38 +0530

    created show page components and populated them with data

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,68 +1,99 @@
-import React, { useEffect,useReducer } from 'react'
-import { useParams } from 'react-router-dom'
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect, useReducer } from 'react';
+import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
+import ShowMainData from '../components/show/ShowMainData';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import Cast from '../components/show/Cast';
 
-const reducer = (prevState,action) => {
-  switch(action.type) {
+const reducer = (prevState, action) => {
+  switch (action.type) {
     case 'FETCH_SUCCESS': {
-      return {isLoading:false, error:null, show:action.show }
+      return { isLoading: false, error: null, show: action.show };
     }
 
     case 'FETCH_FAILED': {
-      return {...prevState, isLoading:false, error:action.error }
+      return { ...prevState, isLoading: false, error: action.error };
     }
 
-    default: return prevState;
+    default:
+      return prevState;
   }
-}
+};
 
 const initialState = {
   show: null,
   isLoading: true,
-  error: null
-}
+  error: null,
+};
 
 const Show = () => {
-  
-  const {id} = useParams();
+  const { id } = useParams();
 
-  const [{show, isLoading, error},dispatch] = useReducer (reducer, initialState)
-  
-  
-  useEffect ( ()=> {
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
-    let isMounted=true
-    
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-        if(isMounted) {
-          dispatch( {type: 'FETCH_SUCCESS', show:results} )
+  useEffect(() => {
+    let isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
         }
-    })
-    .catch(err => {
-      if(isMounted) {
-        dispatch( {type: 'FETCH_SUCCESS', error:err.message} )
-      }
-    })
+      })
+      .catch(err => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
+        }
+      });
 
     return () => {
-      isMounted=false;
-    }
+      isMounted = false;
+    };
+  }, [id]);
 
-  },[id])
-
-  // eslint-disable-next-line no-console
-  console.log ('show',show);
-  if(isLoading) {
-    return <div>Data is being loaded</div>
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
   }
 
-  if(error) {
-    return <div>Error occurred: {error}</div>
+  if (error) {
+    return <div>Error occured: {error}</div>;
   }
-  
+
   return (
-    <div>This is show page.</div>
-  )
-}
+    <div>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
 
-export default Show
+      <div>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </div>
+
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </div>
+
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </div>
+    </div>
+  );
+};
+
+export default Show;
